55f5d1e09c89b9de8b93d631e788df9ed4f4269f
Henrik Adler <henrik.adler@gmail.com>
2017-11-09 00:11:11 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  colors: {
+    white: '#FFFFFF',
+    black: '#000000',
+    mora: '#FF3334',
+    moraShadow: '#DA2B2C',
+    stockholm: '#1C7CE5',
+    stockholmShadow: '#165DAA',
+    ystad: '#F5F5F5',
+    kalmar: '#E6E6E6',
+    karlstad: '#A3A3A3',
+    haparanda: '#323232',
+    jokkmokk: '#1B1B1B',
+    sundsvall: '#5a5a5a',
+    checkmark: '#75AE3C'
+  },
+  space: function (size) {
+    return 8 * size + (size !== 0 ? 'px' : '')
+  }
+}
